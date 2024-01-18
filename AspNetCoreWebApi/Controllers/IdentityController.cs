a62751db54e51860ea2b89148ae97fb4d8e812dd
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-01-18 15:16:54 -0600

    commit

--- AspNetCoreWebApi/Controllers/IdentityController.cs
+++ AspNetCoreWebApi/Controllers/IdentityController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace AspNetCoreWebApi.Controllers
@@ -69,9 +73,9 @@ namespace AspNetCoreWebApi.Controllers
 
                 if (result.Succeeded)
                 {
-                    // User successfully logged in
-                    // Handle success, e.g., generate a token, set authentication cookies, or return user data
-                    return Ok(new { Message = "User logged in successfully" });
+                    var user = await _userManager.FindByNameAsync(model.UserName);
+                    var token = GenerateJwtToken(user);
+                    return Ok(new { Token = token, Message = "User logged in successfully" });
                 }
                 else
                 {
@@ -91,5 +95,26 @@ namespace AspNetCoreWebApi.Controllers
             await _signInManager.SignOutAsync();
             return Ok(new { Message = "User logged out successfully" });
         }
+
+
+        private string GenerateJwtToken(IdentityUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("YourSuperSecretSecurityKey"); // Replace with a secure key
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+            new Claim(ClaimTypes.Name, user.UserName)
+                    // Add other claims as needed
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+
     }
 }
--- AspNetCoreWebApi/obj/Debug/net8.0/AspNetCoreWebApi.AssemblyInfo.cs
+++ AspNetCoreWebApi/obj/Debug/net8.0/AspNetCoreWebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bdafb35904a3346d851ff6dfbe858d8c923ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0d6a6618b4a2146e9e58bd6a2b119038c280e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
